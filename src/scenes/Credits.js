a05af405621ca94cdd416ac4536fe647057706cc
hmedina3 <hmedina3@ucsc.edu>
2020-06-06 15:13:33 -0700

    Added timer to credit scene. - H.

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -29,13 +29,31 @@ class Credits extends Phaser.Scene {
         this.madeByText3.setFontSize('28px');
         this.madeByText4 = this.add.text(50, 400, "Even given the circumstances of the year 2020, we were able to push through and finish this game.", textConfig);
         this.madeByText4.setFontSize('16px');
-        this.madeByText4 = this.add.text(50, 420, "Let us look forward to the future to advance humanity and work towards equality for all.", textConfig);
+        this.madeByText4 = this.add.text(50, 420, "Let us look towards the future to advance humanity and work towards equality for all.", textConfig);
         this.madeByText4.setFontSize('16px');
 
-
-
-
+        // Time for credit scene
+        game.settings.gameTimer = 30000;
+        this.timer = this.formatTime(game.settings.gameTimer);
+        this.timerLeft = this.add.text(30,525,this.timer,textConfig);
+        let timeInSeconds;
+        timeInSeconds = this.time.addEvent({delay:1000, callback: this.onEvent, callbackScope: this, loop:true})
+       
 
     } // end of create function.
+    
+    // More Time UI 
+    formatTime(milliseconds){
+        return milliseconds / 1000;
+    }
+    onEvent(){
+        if(this.timer > 0){
+            this.timer -= 1;
+        }
+        else{
+            this.scene.start("optionsScene");
+        }
+        this.timerLeft.setText(this.timer);
+    }
      
 } // end of credits class.
